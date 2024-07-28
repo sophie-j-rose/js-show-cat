97568885c0767ae967659f38732f875f7c46682a
sophie-rose <sophiejoannerose@gmail.com>
2024-07-27 20:18:45 -0400

    Tested if/else statements

--- js/script.js
+++ js/script.js
@@ -4,5 +4,16 @@ var cat = document.querySelector(".cat");
 console.log(cat);
 button.addEventListener("click", function () {
     //console.log("It's working");
-    cat.classList.add("show");
+    //cat.classList.add("show");
+    if (cat.classList.contains("show")) {
+        //console.log("Yes");
+        cat.classList.remove("show");
+        button.innerText = "Wait, come back!";
+        button.classList.add("disappear");
+    } else {
+        cat.classList.add("show");
+        button.innerText = "Shoo, cat!";
+        button.classList.remove("disappear");
+        //console.log("No");
+    }
 });
